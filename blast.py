2e47b8ae8a71027257061ba1c41c010d9b992a55
ZayerWang <iyw1@miami.edu>
2024-11-01 13:53:56 -0400

    First submission

---
+++ blast.py
@@ -0,0 +1,59 @@
+from Bio import SeqIO
+from Bio.Blast.Applications import NcbiblastpCommandline
+from Bio.Blast import NCBIXML
+
+human_dict = []
+for seq_record in SeqIO.parse("human.fa", 'fasta'):
+    human_dict.append(seq_record.id)
+
+blastp_cline = NcbiblastpCommandline(
+    cmd = "/Users/isaiahwang/BiomedicalDataScience/homework-3-ZayerWang/ncbi-blast-2.16.0+/bin/blastp", 
+    query = "human.fa", 
+    db = "mouse_db", 
+    evalue = 5 * pow(10,-13), 
+    matrix = "PAM30", 
+    outfmt = 5,
+    out = "blast_result.xml"
+    )
+
+'''
+Chose blastp as we are querying protein sequences, thus we should be using blastp (protein) instead of blastn (nucleotide)
+The set evalue and substitution matrix (1*10^-12 and PAM30) were chosen through testing.
+My goal was to output around 100 or less of the closest homologous sequences. 
+Through testing, it was found that some sequences in the query have a lot of similar matches, 
+depending on the parameters I could get up to a range of 300 for number of sequences that met the parameter requirements.
+This was true for all the BLOSUM matrices I tested, and for any evalue with magnitude value greater than 10^-12
+PAM30 (or PAM70) with a evalue in the magnitude of 10^-13 gave me the desired results
+
+Additionally, such a low evalue and using PAM makes sense, as while mice and humans are very different,
+genetivally they should be fairly similar. Both are mammals that split recently (evolutionarily), 
+PAM is usually used for higher matching sequences and the lower the evalue the more closely the sequences match.
+Thus for such similar sequences, using PAM and a very low evalue makes sense.
+'''
+
+blastp_cline()
+
+result_handle = open("blast_result.xml")
+blast_records = NCBIXML.parse(result_handle)
+
+outputFile = open("results.txt", 'w')
+
+index = 0
+for blast_record in blast_records:
+    outputFile.write("Alignments for Human Sequence ID: " + str(human_dict[index]) + "\n")
+    numPrints = 0
+    for alignment in blast_record.alignments:
+        for hsp in alignment.hsps:
+            split = alignment.title.split("|") 
+            outputFile.write("\tMouse Sequence ID: " + str(split[3]) + "\n")
+            outputFile.write("\tQuery sequence:          " + str(hsp.query) + "\n")
+            outputFile.write("\tCorresponding alignment: " + str(hsp.match) + "\n")
+            outputFile.write("\tE-value: " + str(hsp.expect) + "\n")
+            outputFile.write("\tBit Score: " + str(hsp.score) + "\n\n")
+            numPrints += 1
+    
+    print(numPrints)
+    index += 1
+
+
+outputFile.close()
