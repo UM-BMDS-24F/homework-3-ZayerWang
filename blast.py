7f251e2fd121233b9908e41f2e431e8028b147e4
ZayerWang <iyw1@miami.edu>
2024-11-03 21:05:43 -0500

    Final Submission

--- blast.py
+++ blast.py
@@ -23,16 +23,17 @@ blastp_cline = NcbiblastpCommandline(
 '''
 Chose blastp as we are querying protein sequences, thus we should be using blastp (protein) instead of blastn (nucleotide)
 The set evalue and substitution matrix (1*10^-12 and PAM30) were chosen through testing.
-My goal was to output around 100 or less of the closest homologous sequences. 
-Through testing, it was found that some sequences in the query have a lot of similar matches, 
-depending on the parameters I could get up to a range of 300 for number of sequences that met the parameter requirements.
-This was true for all the BLOSUM matrices I tested, and for any evalue with magnitude value greater than 10^-12
-PAM30 (or PAM70) with a evalue in the magnitude of 10^-13 gave me the desired results
+My goal was to output (around) 100 or less of the closest homologous sequences (if only 1 or 2 were very close, then only output those 1 or 2). 
+Through testing, I found that some sequences in the query have a lot of similar matches while other has far fewer. 
+Depending on the parameters, I could get up to a range of 300 for number of sequences that met the evalue threshold.
+This was true for all the BLOSUM matrices I tested, and for any evalue with magnitude value greater than 10^-12.
+PAM30 (or PAM70) with a evalue in the magnitude of 10^-13 gave me the desired result of 100 max homologous sequences 
 
-Additionally, such a low evalue and using PAM makes sense, as while mice and humans are very different,
-genetivally they should be fairly similar. Both are mammals that split recently (evolutionarily), 
-PAM is usually used for higher matching sequences and the lower the evalue the more closely the sequences match.
-Thus for such similar sequences, using PAM and a very low evalue makes sense.
+Additionally from a holistic view, such a low evalue and using PAM makes sense. While mice and humans are very different,
+genetically they should be relatively similar. Both are mammals that split recently (evolutionarily), 
+PAM is usually for closely related (and global) alignments and the lower the evalue the more closely the sequences must match.
+Lots of the human and mouse protein sequences should align (globally) and so a substitution matrix that is used for high global alignment makes sense in context.
+Since these sequences should be fairly similar, we want to lower the evalue so only the highest of alignments will be presented.
 '''
 
 blastp_cline()
